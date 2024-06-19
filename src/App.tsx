059effc7f3408d8909b9379833c331661a045625
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-06-19 23:52:12 +0100

    summary

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import images from "index.js";
 import Addons from "./components/Addons.js";
 import PersonalInfo from "./components/PersonalInfo.js";
 import SelectPlan from "./components/SelectPlan.js";
+import Summary from "./components/Summary.js";
 import { useMultiStepForm } from "./utils/useMultiStepForm.js";
 
 function App() {
@@ -10,11 +11,11 @@ function App() {
     <div>two</div>,
   ]);
   return (
-    <section className=" bg-magnolia  sm:h-screen  grid sm:place-items-center">
+    <section className="grid bg-magnolia sm:h-screen sm:place-items-center">
       <div className="font-ubuntu sm:flex max-h-[568px] justify-center relative sm:p-4 sm:bg-white rounded-xl">
         {/* steps */}
         <div className="sm:bg-[url('assets/images/bg-sidebar-desktop.svg')] bg-[url('assets/images/bg-sidebar-mobile.svg')]  sm:pt-8 px-5 pr-[min(6vw,10em)]  bg-no-repeat bg-cover bg-[position:50%_100%] sm:rounded-lg ">
-          <div className=" flex sm:flex-col items-baseline pt-7 h-44 justify-center sm:gap-5">
+          <div className="flex items-baseline justify-center sm:flex-col pt-7 h-44 sm:gap-5">
             <div className="flex items-center gap-3">
               <h2
                 className={` w-5 h-5 inline-flex items-center justify-center p-4 rounded-full  ${
@@ -26,10 +27,10 @@ function App() {
                 1
               </h2>
               <div>
-                <h2 className=" text-pastel-blue text-xs hidden sm:block">
+                <h2 className="hidden text-xs text-pastel-blue sm:block">
                   STEP 1
                 </h2>
-                <h2 className="text-alabaster font-bold text-sm hidden w-max sm:block">
+                <h2 className="hidden text-sm font-bold text-alabaster w-max sm:block">
                   YOUR INFO
                 </h2>
               </div>
@@ -45,10 +46,10 @@ function App() {
                 2
               </h2>
               <div>
-                <h2 className=" text-pastel-blue text-xs hidden sm:block">
+                <h2 className="hidden text-xs text-pastel-blue sm:block">
                   STEP 2
                 </h2>
-                <h2 className="text-alabaster font-bold text-sm hidden w-max sm:block">
+                <h2 className="hidden text-sm font-bold text-alabaster w-max sm:block">
                   SELECT PLAN
                 </h2>
               </div>
@@ -64,10 +65,10 @@ function App() {
                 3
               </h2>
               <div>
-                <h2 className=" text-pastel-blue text-xs hidden sm:block">
+                <h2 className="hidden text-xs text-pastel-blue sm:block">
                   STEP 3
                 </h2>
-                <h2 className="text-alabaster font-bold text-sm hidden sm:block">
+                <h2 className="hidden text-sm font-bold text-alabaster sm:block">
                   ADD-ONS
                 </h2>
               </div>
@@ -83,10 +84,10 @@ function App() {
                 4
               </h2>
               <div>
-                <h2 className=" text-pastel-blue text-xs hidden sm:block">
+                <h2 className="hidden text-xs text-pastel-blue sm:block">
                   STEP 4
                 </h2>
-                <h2 className="text-alabaster font-bold text-sm hidden sm:block">
+                <h2 className="hidden text-sm font-bold text-alabaster sm:block">
                   SUMMARY
                 </h2>
               </div>
@@ -95,10 +96,11 @@ function App() {
         </div>
         {/* form */}
         <div className="px-[min(3vw,8em)] mt-[min(2vw,4em)]  h-full mx-auto w-full  max-w[560px] sm:bg-transparent  mb-8 sm:mb-0 bg-[#F0F8FF]">
-          <form className=" flex flex-col gap-12  w-full h-full sm:px-4">
+          <form className="flex flex-col w-full h-full gap-12 sm:px-4">
             {/* <PersonalInfo /> */}
             {/* <SelectPlan /> */}
-            <Addons />
+            {/* <Addons /> */}
+            <Summary />
             <button className=" mt-[min(5vw,4em)]  ml-auto bg-marine-blue hover:bg-blue-800 font-semibold text-white w-fit px-6 py-2 mb-2 rounded-md">
               Next Step
             </button>
---
+++ src/components/Summary.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Summary = () => {
+  return (
+    <>
+      <div className="flex w-full flex-col p-7  sm:bg-white bg-alabaster mt-[-5.15em] rounded-lg sm:rounded-none  sm:mt-0 sm:p-0 shadow-lg sm:shadow-none ">
+        <h1 className="text-2xl font-bold text-marine-blue font-ubuntu text sm:text-4xl">
+          Finishing up
+        </h1>
+        <p className="mt-2 text-cool-gray">
+          Double-check everything looks OK before confirming.
+        </p>
+        <div className="flex flex-col gap-4 mt-8 ">
+          <div className="flex flex-col p-6 rounded-lg gap-y-4 bg-gray-50">
+            <div className="flex items-center justify-between">
+              <div>
+                <h2 className="font-bold text-marine-blue">Arcade (Monthly)</h2>
+                <p className="text-sm hover:underline hover:cursor-pointer text-purplish-blue">
+                  Change
+                </p>
+              </div>
+              <p className="font-bold text-marine-blue ">$9/mo</p>
+            </div>
+            <hr />
+            <div className="flex justify-between">
+              <p className="text-sm">Online service</p>
+              <p className="text-sm font-semibold text-marine-blue">+$1/mo</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-sm">Larger storage</p>
+              <p className="text-sm font-semibold text-marine-blue">+$2/mo</p>
+            </div>
+          </div>
+          <div className="flex justify-between px-6">
+            <p className="text-sm ">Total (per month)</p>
+            <p className="text-lg font-bold text-purplish-blue">+$12/mo</p>
+          </div>
+          {/*  */}
+          {/* <div className="flex items-center flex-grow gap-3 p-3 border rounded-md sm:flex-col sm:gap-0 basis-24 sm:items-start">
+            <div className="">
+              <h2 className="font-bold sm:mt-9 text-marine-blue">Arcade</h2>
+              <p className="text-sm font-semibold text-cool-gray">$9/mo</p>
+            </div>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Summary;
